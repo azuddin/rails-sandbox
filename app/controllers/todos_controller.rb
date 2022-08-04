1438a45e91bd4736e8b5cfd73e02d5e1c9f6eada
sandren-svg <70704836+sandren-svg@users.noreply.github.com>
2022-08-04 16:28:50 +0800

    update todo (#2)
    * update `users`
    
    * push todo
    
    * fix todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,19 @@
 class TodosController < ApplicationController
+    def index
+        @todo = todo.all 
+        render json: todoSerializer.new(@todo).serializable_hash.to_json
+    end
+    def show
+        @todo = todo.find(params[:id])
+        render json: todoSerializer.new(@todo).serializable_hash.to_json
+    end
+    def update
+        @todo = todo.find(params[:id])
+        @todo.update todo_params
+        render json: todoSerializer.new(@todo).serializable_hash.to_json
+    end
+    def destroy
+        todo.find(params[:id]).destroy
+        render json: {message: 'OK' }.to_json, status: 200
+     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     render json: {message: 'OK' }.to_json, status: 200
   end
 
-  private
+private
 
   def user_params
     params.permit(
