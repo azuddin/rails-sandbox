69400d86f7bcf1874d53d46bae95254971f81e8e
sandren-svg <70704836+sandren-svg@users.noreply.github.com>
2022-08-04 15:45:37 +0800

    update `users` (#1)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,35 @@
 class UsersController < ApplicationController
   def index
-  end
-  
-  def show
-  end
-  
-  def create
-  end
-  
-  def update
-  end
-  
-  def destroy
-  end
+    @user = user.all 
+    render json: userSerializer.new(@user).serializable_hash.to_json
+end
+def show
+    @user = user.find(params[:id])
+    render json: userSerializer.new(@user).serializable_hash.to_json
+end
+def update
+    @user = user.find(params[:id])
+    @user.update user_params
+    render json: userSerializer.new(@user).serializable_hash.to_json
+end
+def destroy
+    user.find(params[:id]).destroy
+    render json: {message: 'OK' }.to_json, status: 200
+ end
+ 
 
-  private
 
+
+private
+
+def user_params
+    params.permit(
+        :username,
+        :email,
+        :password
+
+    )
+end
   
 
 end
